a8d9f7b445008e9643a7d37a9e2fe827185bdc3f
Rushikesh Ghogare <rushighogare5561@gmail.com>
2024-08-03 19:15:03 +0530

    Environment setup for backend

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const express=require
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv"
+import connectDB from "./utils/db.js";
+dotenv.config();                  //{} --> removed from bracket
+
+const app=express();
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions={
+    origin: 'http//localhost:5173',
+    credentials: true
+}
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})
