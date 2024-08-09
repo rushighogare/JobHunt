91a79fbdbcb2f88e9ef127a315fc745f32d2dabf
Rushikesh Ghogare <rushighogare5561@gmail.com>
2024-08-09 23:55:40 +0530

    Job controller is created

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv"
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/userRoute.js"
 import companyRoute from "./routes/companyRoute.js"
+import jobRoute from "./routes/jobRoute.js"
+
 dotenv.config();                  //{} --> removed from bracket
 
 const app=express();
@@ -25,6 +27,7 @@ const PORT = process.env.PORT || 3000;
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
 
 app.listen(PORT, ()=>{
     connectDB();
