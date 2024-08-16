1d6e5d2292dfedade6b8de9cc8aacb83fff60969
Rushikesh Ghogare <rushighogare5561@gmail.com>
2024-08-16 15:20:02 +0530

    Home page created

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel';
+import { Button } from './ui/button';
 
 const category=[
     "Frontend Developer",
@@ -10,8 +12,22 @@ const category=[
 
 const CategoryCarousel=()=>{
   return (
-    <div>CategoryCarousel</div>
-  )
+    <div>
+      <Carousel className="w-full max-w-xl mx-auto my-20">
+        <CarouselContent>
+            {
+              category.map((cat, index)=>(
+                <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+                  <Button className="rounded-full" variant="outline">{cat}</Button>
+                </CarouselItem>
+              ))
+            }
+        </CarouselContent>
+        <CarouselPrevious/>
+        <CarouselNext/>
+      </Carousel>
+    </div>
+  );
 }
 
 export default CategoryCarousel
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
+import CategoryCarousel from './CategoryCarousel'
+import LatestJobs from './LatestJobs'
+import Footer from './Footer'
 
 function Home() {
   return (
     <div>
         <Navbar/>
         <HeroSection/>
-        {/* <CategoryCarousel/>
+         <CategoryCarousel/>
         <LatestJobs/>
-        <Footer/> */}
+        <Footer/>
     </div>
   )
 }
