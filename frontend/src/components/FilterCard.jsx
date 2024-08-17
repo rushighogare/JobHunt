a871ac5c5d1556ec09bc4abb2414719310542a48
Rushikesh Ghogare <rushighogare5561@gmail.com>
2024-08-17 10:54:30 +0530

    Browse page designed

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { RadioGroup, RadioGroupItem } from './ui/radio-group'
+import { Label } from './ui/label'
+
+const filterData=[
+  {
+    filterType: "Location",
+    array:["Delhi NCR", "Bangalore", "Hyderabad", "Pune", "Mumbai"]
+  },
+  {
+    filterType: "Industry",
+    array: ["Frontend Developer", "Backend Developer", "FullStack Developer"]
+  },
+  {
+    filterType: "Salary",
+    array: ["0-40k", "42k-1lakh", "1lakh to 5lakh"]
+  },
+]
 
 function FilterCard() {
   return (
-    <div>FilterCard</div>
+    <div className='w-full bg-white p-3 rounded-md'>
+      <h1>Filter Jobs</h1>
+      <hr className='mt-3'/>
+      <RadioGroup>
+        {
+          filterData.map((data, index)=>(
+            <div>
+              <h1 className='font-bold text-lg'>{data.filterType}</h1>
+              {
+                data.array.map((item, index)=>{
+                  return (
+                    <div className='flex items-center space-x-2 my-2'>
+                      <RadioGroupItem value={item}/>
+                      <Label>{item}</Label>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          ))
+        }
+      </RadioGroup>
+    </div>
   )
 }
 
