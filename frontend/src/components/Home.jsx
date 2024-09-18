f34e9997da54e111b3e87ef0b74a530e0a4f4847
Rushikesh Ghogare <rushighogare5561@gmail.com>
2024-09-18 19:31:08 +0530

    Home page is dynamic

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,8 +4,10 @@ import HeroSection from './HeroSection'
 import CategoryCarousel from './CategoryCarousel'
 import LatestJobs from './LatestJobs'
 import Footer from './Footer'
+import useGetAllJobs from '@/hooks/useGetAllJobs'
 
 function Home() {
+  useGetAllJobs();
   return (
     <div>
         <Navbar/>
--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import LatestJobCards from './LatestJobCards';
+import { useSelector } from 'react-redux';
 
-const randomJobs=[1, 2, 3, 4, 5, 6, 7, 8];
+// const randomJobs=[1, 2, 3, 4, 5, 6, 7, 8];
 function LatestJobs() {
+  const {allJobs}=useSelector(store=>store.job); 
   return (
     <div className='max-w-7xl mx-auto my-20'>
         <h1 className='text-4xl font-bold'><span className='text-[#6A38C2]'>Latest & Top</span> Job Openings</h1>
         <div className='grid grid-cols-3 gap-4 my-5'>
             {
-                randomJobs.slice(0, 6).map((item, index)=><LatestJobCards/>)              //slice gives you only 0 to 6 elements in this specific case
+                allJobs.length<=0 ? <span>No jobs available</span> : allJobs?.slice(0, 6).map((job)=><LatestJobCards key={job._id} job={job}/>)              //slice gives you only 0 to 6 elements in this specific case
             } 
         </div>
     </div>
