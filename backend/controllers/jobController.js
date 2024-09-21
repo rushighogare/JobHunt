86916d2335e399467c715b1c5b81f05835f4d3d3
Rushikesh Ghogare <rushighogare5561@gmail.com>
2024-09-21 11:57:10 +0530

    Job description is dynamic

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -69,10 +69,12 @@ export const getAllJobs=async(req, res)=>{
 export const getJobById=async(req, res)=>{
     try{
         const jobId=req.params.id;
-        const job=await Job.findById(jobId);
+        const job=await Job.findById(jobId).populate({
+            path:"applications"
+        });
 
         if(!job){
-            return res.status(400).json({
+            return res.status(404).json({
                 message: "Job not found",
                 success: false,
             })
--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,19 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Badge } from './ui/badge';
 import { Button } from './ui/button';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { JOB_API_END_POINT } from '@/utils/constant';
+import { APPLICATION_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant';
 import { setSingleJob } from '@/redux/jobSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'sonner';
 
 function JobDescription() {
-    const isApplied=true;
     const params=useParams();
     const jobId=params.id;
     const {singleJob}=useSelector(store=>store.job);
     const {user}=useSelector(store=>store.auth);
+    const isInitiallyApplied=singleJob?.applications?.some(application=>application.applicant==user?._id) || false;
+    const [isApplied, setIsApplied]=useState(isInitiallyApplied);
     const dispatch=useDispatch();
+
+    const applyJobHandler=async()=>{
+        try{
+            const res=await axios.get(`${APPLICATION_API_END_POINT}/apply/${jobId}`, {withCredentials: true});
+            if(res.data.success){
+                setIsApplied(true);   //update the local state
+                const updatedSingleJob={...singleJob, applications:[...singleJob.applications, {applicant:user?._id}]}; 
+                dispatch(setSingleJob(updatedSingleJob));   //helps us to real time UI update
+                toast.success(res.data.message);
+            }
+        }catch(error){
+            toast.error(error.response.data.message);
+        }
+    }
 
     useEffect(()=>{
         const fetchSingleJob=async()=>{
@@ -21,6 +37,7 @@ function JobDescription() {
                 const res=await axios.get(`${JOB_API_END_POINT}/get/${jobId}`, {withCredentials:true});
                 if(res.data.success){
                     dispatch(setSingleJob(res.data.job));
+                    setIsApplied(res.data.job.applications.some(application=>application.applicant==user?._id));  //ensure the state is in sync with fetched data 
                 }
             }catch(error){
                 console.log(error);
@@ -40,7 +57,8 @@ function JobDescription() {
                     <Badge className={'text-[#7209b7] font-bold'} variant="ghost">{singleJob?.salary} LPA</Badge>
                 </div>
             </div>
-            <Button
+            <Button 
+            onClick={isApplied ? null : applyJobHandler}
             disabled={isApplied}
             className={`rounded-lg ${isApplied ? 'bg-gray-600 cursor-not-allowed' : 'bg-[#7269b7] hover:bg-[#5f32ad]'}`}>
                 {isApplied? 'Already Applied' : 'Apply Now'}
